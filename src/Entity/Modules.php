48edc169b287f81ce1604666393ad25fb2906276
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-03-22 14:21:08 +0100

    Ajout d'entites avec relations

---
+++ src/Entity/Modules.php
@@ -0,0 +1,241 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ModulesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ModulesRepository::class)
+ */
+class Modules
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Blocs::class, inversedBy="modules")
+     */
+    private $bloc;
+
+    /**
+     * @ORM\Column(type="datetime_immutable", nullable=true)
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $created_by;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Intervenants::class, mappedBy="modules")
+     */
+    private $intervenants;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Etudiants::class, mappedBy="modules")
+     */
+    private $etudiants;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Absences::class, mappedBy="module")
+     */
+    private $absences;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Documents::class, mappedBy="module")
+     */
+    private $documents;
+
+   
+
+    public function __construct()
+    {
+        $this->intervenants = new ArrayCollection();
+        $this->etudiants = new ArrayCollection();
+        $this->absences = new ArrayCollection();
+        $this->documents = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getBloc(): ?Blocs
+    {
+        return $this->bloc;
+    }
+
+    public function setBloc(?Blocs $bloc): self
+    {
+        $this->bloc = $bloc;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?string
+    {
+        return $this->created_by;
+    }
+
+    public function setCreatedBy(?string $created_by): self
+    {
+        $this->created_by = $created_by;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Intervenants>
+     */
+    public function getIntervenants(): Collection
+    {
+        return $this->intervenants;
+    }
+
+    public function addIntervenant(Intervenants $intervenant): self
+    {
+        if (!$this->intervenants->contains($intervenant)) {
+            $this->intervenants[] = $intervenant;
+            $intervenant->addModule($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIntervenant(Intervenants $intervenant): self
+    {
+        if ($this->intervenants->removeElement($intervenant)) {
+            $intervenant->removeModule($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Etudiants>
+     */
+    public function getEtudiants(): Collection
+    {
+        return $this->etudiants;
+    }
+
+    public function addEtudiant(Etudiants $etudiant): self
+    {
+        if (!$this->etudiants->contains($etudiant)) {
+            $this->etudiants[] = $etudiant;
+            $etudiant->addModule($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEtudiant(Etudiants $etudiant): self
+    {
+        if ($this->etudiants->removeElement($etudiant)) {
+            $etudiant->removeModule($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Absences>
+     */
+    public function getAbsences(): Collection
+    {
+        return $this->absences;
+    }
+
+    public function addAbsence(Absences $absence): self
+    {
+        if (!$this->absences->contains($absence)) {
+            $this->absences[] = $absence;
+            $absence->setModule($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAbsence(Absences $absence): self
+    {
+        if ($this->absences->removeElement($absence)) {
+            // set the owning side to null (unless already changed)
+            if ($absence->getModule() === $this) {
+                $absence->setModule(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Documents>
+     */
+    public function getDocuments(): Collection
+    {
+        return $this->documents;
+    }
+
+    public function addDocument(Documents $document): self
+    {
+        if (!$this->documents->contains($document)) {
+            $this->documents[] = $document;
+            $document->setModule($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDocument(Documents $document): self
+    {
+        if ($this->documents->removeElement($document)) {
+            // set the owning side to null (unless already changed)
+            if ($document->getModule() === $this) {
+                $document->setModule(null);
+            }
+        }
+
+        return $this;
+    }
+
+}
