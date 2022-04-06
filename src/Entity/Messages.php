1bc1f2f5ce78c4dcb938388caae18bbae1224123
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-04-06 02:50:17 +0100

    Backend/Frontend: Messagerie

--- src/Entity/Messages.php
+++ src/Entity/Messages.php
@@ -37,7 +37,7 @@ class Messages
     /**
      * @ORM\Column(type="boolean")
      */
-    private $is_read;
+    private $is_read = 0;
 
     /**
      * @ORM\ManyToOne(targetEntity=Users::class, inversedBy="sent")
@@ -50,6 +50,12 @@ class Messages
      * @ORM\JoinColumn(nullable=false)
      */
     private $recipient;
+
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTime();
+    }
 
     public function getId(): ?int
     {
