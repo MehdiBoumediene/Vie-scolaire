f7edb4e35cd1b65c77ed11dbc8ff27d39f7955c2
Adiba_Boufeldja <boufeldja.adiba@ifde-learning.fr>
2022-06-08 12:52:39 +0100

    Backend/FrontEnd : parcours pro pour l'apprenant

--- src/Controller/ParcoursApprenantController.php
+++ src/Controller/ParcoursApprenantController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Parcours;
+use App\Form\ParcoursType;
+use App\Repository\ParcoursRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class ParcoursApprenantController extends AbstractController
@@ -13,10 +16,70 @@ class ParcoursApprenantController extends AbstractController
      /**
      * @Route("/parcours/apprenant", name="app_parcours_apprenant")
      */
-    public function index(): Response
+    public function index(ParcoursRepository $parcoursRepository): Response
     {
         return $this->render('parcours_apprenant/index.html.twig', [
-            'controller_name' => 'ParcoursApprenantController',
+            'parcours' => $parcoursRepository->findAll(),
         ]);
+    }
+      /**
+     * @Route("/new", name="app_parcours_apprenant_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, ParcoursRepository $parcoursRepository): Response
+    {
+        $parcour = new Parcours();
+        $form = $this->createForm(ParcoursType::class, $parcour);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $parcoursRepository->add($parcour, true);
+
+            return $this->redirectToRoute('app_parcours_apprenant', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->renderForm('parcours_apprenant/new.html.twig', [
+            'parcour' => $parcour,
+            'form' => $form,
+        ]);
+    }
+     /**
+     * @Route("/{id}", name="app_parcours_apprenant_show", methods={"GET"})
+     */
+    public function show(Parcours $parcour): Response
+    {
+        return $this->render('parcours_apprenant/show.html.twig', [
+            'parcour' => $parcour,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_parcours_apprenant_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Parcours $parcour, ParcoursRepository $parcoursRepository): Response
+    {
+        $form = $this->createForm(ParcoursType::class, $parcour);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $parcoursRepository->add($parcour, true);
+
+            return $this->redirectToRoute('app_parcours_apprenant', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('parcours_apprenant/edit.html.twig', [
+            'parcour' => $parcour,
+            'form' => $form,
+        ]);
+    }
+
+   /**
+     * @Route("/{id}", name="app_parcours_apprenant_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Parcours $parcour, ParcoursRepository $parcoursRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$parcour->getId(), $request->request->get('_token'))) {
+            $parcoursRepository->remove($parcour, true);
+        }
+
+        return $this->redirectToRoute('app_parcours_apprenant', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/ParcoursType.php
+++ src/Form/ParcoursType.php
@@ -6,16 +6,17 @@ use App\Entity\Parcours;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ParcoursType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('bio')
-            ->add('experiencePro')
-            ->add('competence')
-            ->add('education')
+            ->add('bio',TextareaType::class)
+            ->add('experiencePro',TextareaType::class)
+            ->add('competence',TextareaType::class)
+            ->add('education',TextareaType::class)
         ;
     }
 
